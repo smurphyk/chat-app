43cabbaaa3532a273aa5ba2d6218b38222ec9fc6
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-08-04 10:17:41 -0500

    Created Gifted Chat interface with keyboard spacer for android users

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, TextInput } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
   constructor(props) {
@@ -8,12 +9,45 @@ export default class Chat extends React.Component {
     // Set default state for adding text field functionality
     this.state = {
       text: '',
+      messages: []
     }
+  }
+
+  // Creates format for all messages
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    })
+  }
+
+  // Allows messages to be appended to the state's messages object
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
   }
 
   render() {
     // Define props passed from start
     const { name, color } = this.props.route.params;
+    const { messages } = this.state;
 
     // Fallback in case no name is entered on start screen
     if (!name || name === '') name = 'Nobody'
@@ -22,18 +56,25 @@ export default class Chat extends React.Component {
     this.props.navigation.setOptions({ title: name });
 
     return (
-      // Sets colorChoice as chat screen background color
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: color }}>
-        <TextInput
-          style={styles.input}
-          placeholder="Hold your horses, I'm working on it!"
+      // Creates Gifted Chat interface with keyboard spacer for android
+      <View style={styles.container}>
+        <GiftedChat
+          messages={messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
         />
+        {Platform.OS === 'android' ? <KeyboardSpacer /> : null}
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
   input: {
     borderWidth: 2,
     borderColor: '#757083',
